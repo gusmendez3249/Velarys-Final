b310a7c563c8001eb76ab7c5597339ceb1205186
Gustavo <gustavoangelc2005@gmail.com>
2024-07-01 23:03:18 -0600

    Fucion con Ivan

--- src/app/velarys/velarys.module.ts
+++ src/app/velarys/velarys.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InicioComponent } from './inicio/inicio.component';
 import { CursoComponent } from './curso/curso.component';
-import { NivelesComponent } from './niveles/niveles.component';
-import { NavbarComponent } from './velarys/navbar/navbar.component';
-import { NivelComponent } from './niveles/nivel/nivel.component';
+
 import { Nivel1Component } from './niveles/nivel1/nivel1.component';
 import { Nivel2Component } from './niveles/nivel2/nivel2.component';
 import { Nivel3Component } from './niveles/nivel3/nivel3.component';
@@ -17,9 +15,6 @@ import { AdminComponent } from './admin/admin.component';
   declarations: [
     InicioComponent,
     CursoComponent,
-    NivelesComponent,
-    NavbarComponent,
-    NivelComponent,
     Nivel1Component,
     Nivel2Component,
     Nivel3Component,
