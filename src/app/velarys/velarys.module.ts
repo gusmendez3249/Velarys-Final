81565f6dff22cd87052974301d92de1be7431d5f
Gustavo <gustavoangelc2005@gmail.com>
2024-07-01 23:56:35 -0600

    Solucion de conflicto Module

--- src/app/velarys/velarys.module.ts
+++ src/app/velarys/velarys.module.ts
@@ -1,30 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { InicioComponent } from './inicio/inicio.component';
-import { CursoComponent } from './curso/curso.component';
-<<<<<<< HEAD
-=======
 
->>>>>>> gustavo
-import { Nivel1Component } from './niveles/nivel1/nivel1.component';
-import { Nivel2Component } from './niveles/nivel2/nivel2.component';
-import { Nivel3Component } from './niveles/nivel3/nivel3.component';
-import { PagoComponent } from './niveles/pago/pago.component';
-import { AdminComponent } from './admin/admin.component';
-import { PreguntasComponent } from './curso/ejercicios/preguntas/preguntas.component';
 
 
 
 @NgModule({
   declarations: [
-    InicioComponent,
-    CursoComponent,
-    Nivel1Component,
-    Nivel2Component,
-    Nivel3Component,
-    PagoComponent,
-    AdminComponent,
-    PreguntasComponent
+
   ],
   imports: [
     CommonModule
